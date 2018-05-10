e849878a9bdf3fe25e37ca848721ef9a355c21ac
Jorge Cornejo <cornejotux@gmail.com>
2018-05-09 18:09:13 -0700

    Now with all the variables recognized and made into a function.

--- WiFOracle/WiFOracle.ino/WiFOracle.ino.ino
+++ WiFOracle/WiFOracle.ino/WiFOracle.ino.ino
@@ -32,16 +32,9 @@ String getValue(String data, char separator, int index)
     return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("WiFOracle");
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-  wifiManager.setConfigPortalTimeout(180);
-
-  Serial.begin(115200);
-
-  // The next lines are the code to make the http request!
+String waveRequest()
+{
+    // The next lines are the code to make the http request!
   HTTPClient http;
   http.begin("http://www.surforacle.net/orbs/007/index.php");
   int httpCode = http.GET();
@@ -54,17 +47,38 @@ void setup() {
   aP = getValue(aP, ' ', 1);
   String wD = getValue(payload, ':', 23);
   String wDl = getValue(wD, '&', 0);
-  wDl = getValue(wDl, '(', 0);
-  String wDn = getValue(wDl, '(', 1);
-  //wDl = getValue(wD, ' ', 1);
-  //wD = wDl + wDn;
+  wDl = getValue(wDl, ' ', 1);
+  String wDn = getValue(wD, '&', 0);
+  wDn = getValue(wDn, '(', 1);
+  wD = wDl + " " + wDn;
+
+  String wT = getValue(payload, ':', 24);
+  wT = getValue(wT, '&', 0);
+  wT = getValue(wT, ' ', 1);
 
   Serial.print("Wave High: ");   Serial.println(wh);
   Serial.print("dom Perio: ");   Serial.println(dP);
   Serial.print("avg Perio: ");   Serial.println(aP);
-  Serial.print("wave Dire: ");   Serial.println(wDn);
+  Serial.print("wave Dire: ");   Serial.println(wD);
+  Serial.print("water Tem: ");   Serial.println(wT);
+ 
+  String data = wh + "-" + dP + "-" + aP + "-" + wD + "-" + wT;
+  return data;
+}
 
+void setup() {
+  // put your setup code here, to run once:
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("WiFOracle");
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  wifiManager.setConfigPortalTimeout(180);
+
+  Serial.begin(115200);
 }
 
 void loop() {
+  String data = waveRequest();
+  Serial.println(data);
+  delay(1000*60*30); //Will wait for 30 minutes
+    
 }

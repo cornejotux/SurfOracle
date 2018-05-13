5f7229a320b44bb5cf10c9e13d0e67b06b7f971d
Jorge Cornejo-Donoso <cornejotux@gmail.com>
2018-05-13 09:27:29 -0700

    Still a work in progress
    Added LCD using the i2c connection and the function to update the color LED.
    Still need some work.

--- WiFOracle/WiFOracle.ino/WiFOracle.ino.ino
+++ WiFOracle/WiFOracle.ino/WiFOracle.ino.ino
@@ -1,14 +1,31 @@
+/* This uses the W1Mos D1 R2 & mini Board
+ *  
+ */
+#include <SPI.h>
+#include <Wire.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <LiquidCrystal_I2C.h>        //To send the info to a LCD Screen
 
+LiquidCrystal_I2C lcd(0x27,16,2); 
 
-const char* host = "www.surforacle.net";
+static uint32_t timer;
 
 //flag for saving data
 bool shouldSaveConfig = false;
+
+String wH = "00";
+double  waveheight=0;
+String dP = "00";
+String aP = "00";
+String wD = "00";
+String wT = "00";
+
+// Initialize pins for the color LED
+int red=5,green=6,blue=7; //
 
 //callback notifying us of the need to save config
 void saveConfigCallback () {
@@ -32,41 +49,41 @@ String getValue(String data, char separator, int index)
     return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-String waveRequest()
+void waveRequest()
 {
     // The next lines are the code to make the http request!
   HTTPClient http;
   http.begin("http://www.surforacle.net/orbs/007/index.php");
   int httpCode = http.GET();
   String payload = http.getString();
-  String wh = getValue(payload, ':', 20);
-  wh = getValue(wh, ' ', 1);
-  String dP = getValue(payload, ':', 21);
+  wH = getValue(payload, ':', 20);
+  wH = getValue(wH, ' ', 1);
+  dP = getValue(payload, ':', 21);
   dP = getValue(dP, ' ', 1);
-  String aP = getValue(payload, ':', 22);
+  aP = getValue(payload, ':', 22);
   aP = getValue(aP, ' ', 1);
-  String wD = getValue(payload, ':', 23);
+  wD = getValue(payload, ':', 23);
   String wDl = getValue(wD, '&', 0);
   wDl = getValue(wDl, ' ', 1);
   String wDn = getValue(wD, '&', 0);
   wDn = getValue(wDn, '(', 1);
   wD = wDl + " " + wDn;
 
-  String wT = getValue(payload, ':', 24);
+  wT = getValue(payload, ':', 24);
   wT = getValue(wT, '&', 0);
   wT = getValue(wT, ' ', 1);
+  waveheight = wH.toFloat();
 
-  Serial.print("Wave High: ");   Serial.println(wh);
+  Serial.print("Wave High: ");   Serial.println(wH);
   Serial.print("dom Perio: ");   Serial.println(dP);
   Serial.print("avg Perio: ");   Serial.println(aP);
   Serial.print("wave Dire: ");   Serial.println(wD);
   Serial.print("water Tem: ");   Serial.println(wT);
- 
-  String data = wh + "-" + dP + "-" + aP + "-" + wD + "-" + wT;
-  return data;
 }
 
 void setup() {
+    lcd.init();
+     lcd.backlight();
   // put your setup code here, to run once:
   WiFiManager wifiManager;
   wifiManager.autoConnect("WiFOracle");
@@ -76,9 +93,72 @@ void setup() {
   Serial.begin(115200);
 }
 
-void loop() {
-  String data = waveRequest();
-  Serial.println(data);
-  delay(1000*60*30); //Will wait for 30 minutes
-    
+
+static void updateLCD()
+  {
+  lcd.clear();
+  lcd.setCursor(0,0);   lcd.print("H:");
+  lcd.setCursor(2,0);   lcd.print(wH);
+  lcd.setCursor(7,0);   lcd.print("P: ");
+  lcd.setCursor(9,0);   lcd.print(dP);
+  lcd.setCursor(11,0);  lcd.print("|");
+  lcd.setCursor(12,0);  lcd.print(aP);
+  lcd.setCursor(0,1);   lcd.print("T:");
+  lcd.setCursor(2,1);   lcd.print(wT);
+  lcd.setCursor(9,1);   lcd.print(wD);
+  lcd.setCursor(7,1);   lcd.print("D:");
+  lcd.display();
+  Serial.println("LCD updated");
+  }
+
+void loop ()
+{
+  //ether.packetLoop(ether.packetReceive());
+
+ if (millis() > timer)
+  {
+
+    Serial.println("\nSending request for page /data/latest_obs/46216.rss");
+    Serial.println("Obtaining RSS info...");
+    waveRequest();
+
+    if (waveheight != 0)
+    {
+      timer = millis() + 1000*60*30;
+      updateLCD();
+      updateColor();
+    }
+    if (waveheight == 0)
+    {
+      timer = millis() + 1000;// every 30 secs
+    }
+  }
+}
+
+static void updateColor()
+{
+  
+//The code below lights up RGB module light depending on the wave height
+if ((waveheight>=0)&&(waveheight<1))
+   {
+     analogWrite(red   , 0  );     analogWrite(green , 0  );     analogWrite(blue  , 255);
+   }
+   if ((waveheight>=1)&&(waveheight<2))
+   {
+     Serial.println(waveheight);
+     analogWrite(red   ,0);     analogWrite(green ,255 );     analogWrite(blue  ,255);
+   };
+      if ((waveheight>=2)&&(waveheight<3))
+   {
+     analogWrite(red   ,0);     analogWrite(green ,255 );     analogWrite(blue  ,0);
+   };
+   if ((waveheight>=3)&&(waveheight<4))
+   {
+     analogWrite(red   ,255);     analogWrite(green ,255 );     analogWrite(blue  ,0);
+   };
+   if ((waveheight>=4)&&(waveheight<100))
+   {
+     analogWrite(red, 255 ); analogWrite(green, 0); analogWrite(blue, 0);
+   }
+  Serial.println("RGB Color updated");
 }
